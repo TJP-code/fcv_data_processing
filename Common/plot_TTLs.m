1bb7dca99598a18ae6b7add14fb53e13521b01c1
MariosPanayi <mariooosp@gmail.com>
2018-01-22 15:03:45 +0000

    Plot TTL Names
    Added ability to input TTL Names when plotting TTLs

--- Common/plot_TTLs.m
+++ Common/plot_TTLs.m
@@ -1,4 +1,4 @@
-function h = plot_TTLs(TTLs, ts)
+function h = plot_TTLs(TTLs, ts, TTLnames)
 %function h = plot_TTLs(TTLs)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -6,10 +6,15 @@ function h = plot_TTLs(TTLs, ts)
 %   plot_TTLs
 %
 %   Takes TTL data read from tarheel file using TTLsRead and plots.
-%   
-%   inputs:    
-%           TTLs - data read from tarheel .txt file using TTLsRead
-%   output: 
+%
+%   inputs:
+%           TTLs - data read from tarheel .txt file using TTLsRead (1 x n Cell array containing i x j (time x TTL number)matrices )
+%           ts   - time stamps from tarheel .txt file using TTLsRead (1 x n matrix)
+%           TTLnames - Cell array of TTL names (1 x 16) e.g.
+%           {'Reward', 'Head Entry', 'Head Exit', 'Left Lever Press', 'Left Lever Out', 'Right Lever Press', 'Right Lever Out', 'Fan', '9', '10', '11', '12', '13', '14', '15', '16'}
+%           OR
+%   Default {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'}
+%   output:
 %           h = handle to figure
 %
 %
@@ -18,6 +23,7 @@ function h = plot_TTLs(TTLs, ts)
 %                   customise title?
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if nargin < 3; TTLnames = []; end;
 
 %Modify TTLs to allow for separate lines
 TTLs_plot = TTLs * 0.5;
@@ -26,4 +32,10 @@ for i = 1:size(TTLs,2)
 end
 
 plot(ts,TTLs_plot)
-title('TTLs');xlim([ts(1),max(ts)]);xlabel('Times(s)');    
+title('TTLs');xlim([ts(1),max(ts)]);xlabel('Times(s)');
+set(gca,'YTick',1:size(TTLs,2));
+
+%Add custom specified labels for each TTL N.B. newer functions used in Matlab 2016a+
+if ~isempty(TTLnames)
+    set(gca,'yticklabel',TTLnames);
+end
