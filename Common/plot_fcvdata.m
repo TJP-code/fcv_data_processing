5b5b89398243876692462163502226c10a9d0bdd
TJPchump <31247752+TJPchump@users.noreply.github.com>
2018-02-08 17:31:23 +0000

    removed title, added plotting scan lines etc

--- Common/plot_fcvdata.m
+++ Common/plot_fcvdata.m
@@ -1,10 +1,38 @@
-function h = plot_fcvdata(data,ts,clim)
+function h = plot_fcvdata(data,ts,lines,clim)
+% function h = plot_fcvdata(data,ts,clim,lines)
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% data  = fcv data
+% ts    = timestamps for fcv data
+%
+% lines.   (data structure containing plotting params)
+%          point_number = point in waveform to draw line
+%          scan_number  = scan to draw line at
+%          bg           = scan to draw bg line
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
     if nargin < 2; ts = []; end;
-    if nargin < 3; clim = []; end;
+    if nargin < 3; lines = []; end;
+    if nargin < 4; clim = []; end;
     if isempty(ts)
         h = imagesc(data);
+        hold on
+        if ~isempty(lines) 
+            %plot white lines for scan and point
+            plot([1 size(data,1)],[lines.point_number,lines.point_number],'w')
+            plot([lines.scan_number,lines.scan_number],[0,size(data,1)],'w')
+            plot([lines.bg,lines.bg],[0,size(data,1)],'b')            
+        end
     else
         h = imagesc(ts,[0:1:size(data,1)],data);
+        hold on
+        if ~isempty(lines) 
+            %plot white lines for scan and point
+            plot([ts(1),max(ts)],[lines.point_number,lines.point_number],'w')
+            plot([lines.scan_number/10,lines.scan_number/10],[0,size(data,1)],'w')
+            plot([lines.bg/10,lines.bg/10],[0,size(data,1)],'b')            
+        end
     end
     load fcv_colormap
     colormap(norm_fcv)
@@ -15,6 +43,6 @@ function h = plot_fcvdata(data,ts,clim)
     ax = gca;
     ax.YDir = 'normal';
     
-    title('Colourplot - Applied waveform Vs Time');xlabel('Time(s)');ylabel('Point Number')
+    xlabel('Time(s)');ylabel('Point Number')
     
-    
+   
