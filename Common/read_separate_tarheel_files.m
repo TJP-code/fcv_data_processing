eae10718e3dc9b4ba1674b64045f5e4a6ce519ba
MariosPanayi <mariooosp@gmail.com>
2018-03-17 20:39:44 +0000

    Added individual ts extraction into cell array

--- Common/read_separate_tarheel_files.m
+++ Common/read_separate_tarheel_files.m
@@ -20,7 +20,7 @@ files([myindices])=[];
 
 
 
-all_ch0_data = []; all_ch1_data = [];all_ttls = [];
+all_ch0_data = []; all_ch1_data = [];all_ttls = []; ts = [];
 %for each datafile
 for i = 1:length(files)
     %initialise variable
@@ -30,6 +30,7 @@ for i = 1:length(files)
     [~, tempTTLs] = TTLsRead([datapath files{i} '.txt']);   
     %save data into separate 
     all_ch0_data{i} = [temp_ch0_fcv_data];
+    ts{i} = [0:0.1:size(temp_ch0_fcv_data,2)/10-0.1]; 
     if no_of_channels == 2
         all_ch1_data{i} = [temp_ch1_fcv_data];
     end
@@ -40,8 +41,8 @@ TTLs = all_ttls;
 ch0_fcv_data = all_ch0_data;
 ch1_fcv_data = all_ch1_data;
 
-%create ts for whole session
-ts = [0:0.1:size(all_ch0_data,2)/10-0.1]; 
+%create ts for whole session N.B. only when all files are the same length!
+%ts = [0:0.1:size(all_ch0_data{1},2)/10-0.1]; 
 
 %Filenames
 fileNames = files;
