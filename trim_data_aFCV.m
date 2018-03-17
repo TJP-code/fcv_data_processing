1b4b7638bcc283667daf30d3ee6be1b39d59b89e
MariosPanayi <mariooosp@gmail.com>
2018-03-17 16:07:06 +0000

    New aFCV filestructure specific functions

---
+++ trim_data_aFCV.m
@@ -0,0 +1,10 @@
+
+function trimmed_data = trim_data_aFCV(cut_data, cut_params)
+
+%Identify start and end cut points in seconds and convert to scan number
+trim_start = (cut_params.trimData(1)*cut_params.sample_rate)+1;
+trim_end = cut_params.trimData(2)*cut_params.sample_rate+1;
+%%bg subtract/plot
+for i = 1:length(cut_data)
+    [trimmed_data{i}] = cut_data{1,i}(:,trim_start:trim_end);
+end
