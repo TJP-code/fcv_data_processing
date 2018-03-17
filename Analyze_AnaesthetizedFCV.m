07023a0bcb9d74f93a751b8770cb579ea5d8b841
MariosPanayi <mariooosp@gmail.com>
2018-03-17 16:06:49 +0000

    Updated new functions

--- Analyze_AnaesthetizedFCV.m
+++ Analyze_AnaesthetizedFCV.m
@@ -1,7 +1,7 @@
 close all
 clear all
 %Set file path
-datapath = 'E:\Marios aFCV\GLRA_002\GLRA002_20170927_GLRA65.2a\02_STIMRESPONSE_VARY_AMPLITUDE\';
+datapath = 'C:\Users\Marios\Desktop\GLRA002_20170927_GLRA65.2a\02_STIMRESPONSE_VARY_AMPLITUDE\';
 
 %Set number of channels
 no_of_channels = 1;
@@ -14,8 +14,8 @@ bg_params.sample_freq = 58820;
 
 
 %chemometric variables
-chemo_params.cv_matrix = dlmread('C:\Users\mpanagi\Documents\GitHub\fcv_data_processing\chemoset\cvmatrix1.txt');
-chemo_params.conc_matrix = dlmread('C:\Users\mpanagi\Documents\GitHub\fcv_data_processing\chemoset\concmatrix1.txt');
+chemo_params.cv_matrix = dlmread('C:\Users\Marios\Documents\GitHub\fcv_data_processing\chemoset\cvmatrix1.txt');
+chemo_params.conc_matrix = dlmread('C:\Users\Marios\Documents\GitHub\fcv_data_processing\chemoset\concmatrix1.txt');
 chemo_params.pcs = [];
 chemo_params.alpha = 0.05;
 chemo_params.plotfigs = 0;
@@ -24,7 +24,7 @@ chemo_params.plotfigs = 0;
 cut_params.sample_rate = 10;
 cut_params.target_pos = 5; %time of stimulation
 cut_params.bg_pos = -0.5; %seconds relative target
-cut_params.croplength = [0 10]; %crop long files between a [start end] time in seconds
+cut_params.trimData = [0 15]; %crop long files between a [start end] time in seconds
 
 
 %visualisation params
@@ -39,26 +39,55 @@ params.fig_title = 'Amplitude Response Curve';
 [fileNames, TTLs, ch0_fcv_data, ~, ts] = read_separate_tarheel_files(datapath, no_of_channels);
 %Background subtract data
 processed_data = bg_subtract_aFCV(ch0_fcv_data, cut_params, bg_params);
+processed_data = trim_data_aFCV(processed_data, cut_params);
 
 %% Apply chemometrics
 
-%initialise variables
-model_cvs = [];
-c_predicted = [];
-residuals = [];
 
 if params.apply_chemometrics
-    %apply chemometrics 
+    %initialise variables
+    model_cvs = [];
+    c_predicted = [];
+    residuals = [];
+    
+    %apply chemometrics
     [model_cvs, c_predicted, residuals.q, residuals.q_crit, residuals.q_cutoff] = ...
-        fcv_chemometrics(processed_data, chemo_params, TTLs, ts);    
+        fcv_chemometrics(processed_data, chemo_params, TTLs, ts);
 end
 
 %% Plot
-h = plot_fcv_trials_Anaesthetized(model_cvs(1,3:4), ts, TTLs, params, c_predicted);
+h = plot_fcv_trials_Anaesthetized(model_cvs, ts, TTLs, params, c_predicted);
 suptitle([params.fig_title]);
 
 
+%calculate max value of DA i.e. DA peak 
+baseline = (cut_params.target_pos + cut_params.bg_pos)*cut_params.sample_rate;
+DA_max = [];
+DA_latency = [];
+for i = 1: size(c_predicted, 2)
+[max_val, max_index] = max(c_predicted{i}(1,baseline:end),[], 2);
+
+DA_max(i) = max_val;
+DA_latency(i) = max_index; %N.b. this is latency from baseline in scan number
+end
 
 
+figure
+scatter([1:length(DA_max)], DA_max)
+figure
+scatter([1:length(DA_latency)], DA_latency)
+
+
+delimiter = '_'; %delimiter used between filename sections
+remove = 1; % Boolean, True = remove the target text from the output, False = leave target text in output
+
+for i = 1:length(fileNames)
+target = 'Hz'; %target text
+stimFreq(i) = str2double(filenameSplitter(fileNames{i},delimiter,target,remove));
+target = 'p'; %target text
+stimPulses{i} = filenameSplitter(fileNames{i},delimiter,target,remove);
+target = 'uA'; %target text
+stimStrength{i} = filenameSplitter(fileNames{i},delimiter,target,remove);
+end
 
 
