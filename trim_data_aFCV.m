bfea502c7bd1541122ad42fce1231c95170458b1
MariosPanayi <mariooosp@gmail.com>
2018-03-17 20:37:41 +0000

    added ts to trim data to avoid errors when plotting

--- trim_data_aFCV.m
+++ trim_data_aFCV.m
@@ -1,5 +1,5 @@
 
-function trimmed_data = trim_data_aFCV(cut_data, cut_params)
+function [trimmed_data, ts] = trim_data_aFCV(cut_data, cut_params, ts)
 
 %Identify start and end cut points in seconds and convert to scan number
 trim_start = (cut_params.trimData(1)*cut_params.sample_rate)+1;
@@ -7,4 +7,5 @@ trim_end = cut_params.trimData(2)*cut_params.sample_rate+1;
 %%bg subtract/plot
 for i = 1:length(cut_data)
     [trimmed_data{i}] = cut_data{1,i}(:,trim_start:trim_end);
+    [ts{i}] = ts{1,i}(:,trim_start:trim_end);
 end
